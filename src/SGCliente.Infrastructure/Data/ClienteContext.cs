0808c463b8619ee9449619c3cc885af5aca49e47
Murilo Olegini <muriloolegini@outlook.com>
2022-06-22 12:54:23 -0300

    Foi adicionado EntityFrameworkCore e mapeado o banco de dados.

---
+++ src/SGCliente.Infrastructure/Data/ClienteContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGCliente.ApplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGCliente.Infrastructure.Data
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Cliente");
+        }
+    }
+}
--- src/SGCliente.UI.Web/Program.cs
+++ src/SGCliente.UI.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SGCliente.Infrastructure.Data;
 using SGCliente.UI.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,13 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+var service = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ClienteContext>(options =>
+    options.UseSqlServer(service));
+
+/*builder.Services.AddDbContext<ClienteContext>(options =>
+    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));*/
 
 var app = builder.Build();
 
