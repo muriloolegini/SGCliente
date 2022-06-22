bd098e9e01e852be453ebf949c2ea389191e3fbb
Murilo Olegini <muriloolegini@outlook.com>
2022-06-22 10:53:53 -0300

    Add project files.

---
+++ src/SGCliente.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGCliente.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+    }
+}
---
+++ src/SGCliente.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SGCliente.UI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
